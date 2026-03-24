d7271672be88511441234821cf5bf79c21f02371
joss21 <josselynj98@gmail.com>
2026-03-24 15:51:36 -0500

    fix: quit route and add zone

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Calculadora } from "./calculadora/calculadora";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Calculadora],
+  imports: [Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
